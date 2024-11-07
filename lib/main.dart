71e4a2907b3bc9b0bec3c2ae5c5a0b89caa0ddc1
samalss <samal200174@gmail.com>
2024-11-07 07:17:47 +0100

    Disabling the DEBUG icon

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Guess the Number',
       theme: ThemeData(
         primarySwatch: Colors.blue,
